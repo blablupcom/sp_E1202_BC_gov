20355b3ec5a307aa9b504b7c1ad2d3e3dc865fe8
blablupcom <o.volodin@yahoo.com>
2017-03-21 23:53:54 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-  #-*- coding: utf-8 -*-
+ #-*- coding: utf-8 -*-
 
 #### IMPORTS 1.0
 import os
@@ -87,6 +87,9 @@ entity_id = "E1202_BC_gov"
 url = "http://www.bournemouth.gov.uk/CouncilDemocratic/AboutYourCouncil/Transparency/PaymentstoSuppliers.aspx?GenericListPaymentstoSuppliers_List_GoToPage={}"
 errors = 0
 data = []
+proxy = urllib2.ProxyHandler({'http': '89.36.223.59:8080'})
+opener = urllib2.build_opener(proxy)
+urllib2.install_opener(opener)
 
 #### READ HTML 1.0
 
